2c50765406e85978033c0ca7f3016c274c5e2114
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-18 16:51:46 -0300

    build(Api): creating request for categories api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
   // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const awaitRequest = await response.json();
+  return awaitRequest;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
