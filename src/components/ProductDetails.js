7868e847dccd9e032996da6f257970c6c825143a
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-19 22:10:04 -0300

    ✨ component feat: [ProductDetails] [creating function product details]

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { PropTypes } from 'prop-types';
+import { getProductById } from '../services/api';
 
 class ProductDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productName: '',
+      img: '',
+      priceProduct: 0,
+      atributtesProduct: [],
+    };
+  }
+
+  componentDidMount() {
+    const { props: { match: { params: { id } } } } = this;
+    this.getProduct(id);
+  }
+
+  getProduct = async (id) => {
+    const response = await getProductById(id);
+    console.log(response);
+    this.setState(({
+      productName: response.title,
+      img: response.thumbnail,
+      priceProduct: response.price,
+      atributtesProduct: [...response.attributes],
+    }));
+  };
+
   render() {
-    const { product, price, img } = this.props;
+    const { productName, img, priceProduct, atributtesProduct } = this.state;
     return (
       <div>
         <Card style={ { width: '18rem' } } data-testid="product">
-          <Card.Img variant="top" src={ img } />
           <Card.Body>
-            <Card.Title>{ product }</Card.Title>
-            <Card.Text>{ price }</Card.Text>
+            <p data-testid="product-detail-name">{ productName }</p>
+            <img src={ img } alt={ productName } />
+            <p>
+              R$
+              {' '}
+              { priceProduct }
+            </p>
+            {atributtesProduct.map((value) => (
+              <div key={ value.id }>
+                <p>
+                  {value.name}
+                  :
+                  {' '}
+                  {value.value_name}
+                </p>
+              </div>
+            ))}
             <Button variant="primary">Adicionar ao carrinho</Button>
           </Card.Body>
         </Card>
@@ -20,10 +61,12 @@ class ProductDetails extends Component {
   }
 }
 
-export default ProductDetails;
-
 ProductDetails.propTypes = {
-  product: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  img: PropTypes.string.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
+
+export default ProductDetails;
